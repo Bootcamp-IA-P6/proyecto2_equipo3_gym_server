aac982757bd98d081c1b3a602194959edd9e4eb7
gemayc <gemita284@hotmail.com>
2026-01-15 13:29:43 +0100

    feat: add required role validation for tokens and permissions #12

--- core/security.py
+++ core/security.py
@@ -1,11 +1,18 @@
 import bcrypt
 
 def hash_password(password: str) -> str:
+    """
+    Recibe una contraseña en texto plano
+    y devuelve un hash seguro.
+    """
     password_bytes = password.encode("utf-8")
     hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed.decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Comprueba si una contraseña coincide con su hash.
+    """
     return bcrypt.checkpw(
         plain_password.encode("utf-8"),
         hashed_password.encode("utf-8")
