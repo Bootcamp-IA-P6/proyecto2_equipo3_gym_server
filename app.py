42810b0b276b85e8345aad53cd524ef746917cef
IrisFernandaAmorim <amorim.irisf@gmail.com>
2026-01-15 12:31:14 +0100

    feat: created basic loggings

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ from models.base import Base
 from routes.users_routes import router as users_router
 from routes.trainers_routes import router as trainers_router
 from routes.classes_routes import router as classes_router
+from config.logger import setup_logger, get_logger
 
 import models.user
 import models.trainer
 import models.gym_class
 import models.user_class
+
+
+setup_logger()
+logger = get_logger("app")
 
 app = FastAPI(
     title="Gym Management API"
@@ -21,4 +26,5 @@ app.include_router(classes_router)
 
 @app.on_event("startup")
 def startup():
+    logger.info("Application started")
     Base.metadata.create_all(bind=engine)
