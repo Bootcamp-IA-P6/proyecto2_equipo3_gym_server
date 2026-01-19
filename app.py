7bb579d09d819840c4a4179ec75d6e31d0627221
gemayc <gemita284@hotmail.com>
2026-01-19 14:57:31 +0100

    feat: add cors middleware for port 3000 #46

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request 
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from config.logger import setup_logger, get_logger
 from config.exceptions import AppException, NotFoundException,InvalidDataException
 
@@ -10,6 +11,7 @@ from routes.user_class_routes import router as user_class_router
 from routes.trainers_routes import router as trainers_router
 from routes.classes_routes import router as classes_router
 from routes.auth_routes import router as auth_router
+from config.settings import settings
 
 
 import models.user
@@ -23,6 +25,18 @@ logger = get_logger("app")
 
 app = FastAPI(
     title="Gym Management API"
+)
+
+# CONFIGURAR CORS CON ENV ---
+# Convertimos el string "url1,url2" en una lista ["url1", "url2"]
+origins = settings.ALLOWED_ORIGINS.split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # Qué dominios pueden hablar con tu API
+    allow_credentials=True,     # Permitir cookies/tokens de autenticación
+    allow_methods=["*"],        # Permitir todos los métodos (GET, POST, PUT, DELETE...)
+    allow_headers=["*"],        # Permitir todos los headers
 )
 
 app.include_router(users_router)
